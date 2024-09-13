31e2c6113446e31d95178a7b6b7402a82fd399ce
Fabscii <129980914+FabianSchieder@users.noreply.github.com>
2024-09-13 12:12:09 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+int main()
+{
+
+  FILE *fp;
+  int i, temp;
+
+  fp = fopen("zahlen.txt", "r");
+
+  if(fp == NULL)
+  {
+    printf("Datei konnte nicht geöffnet werden");
+  }
+  else
+  {
+    for(int i = 0; i < 10; i++)
+      {
+        fscan(fp, "%d\n", &temp);
+        printf("zahl: %d\n", temp);
+      }
+  }
+  
+}
